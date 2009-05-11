18bd39360d4808666bb240c594f8558b3657b0a3
Alan Hourihane <alanh@fairlite.co.uk>
2009-05-11 19:20:40 +0000

    Fix math.h

--- math.h
+++ math.h
@@ -70,9 +70,7 @@ enum fdversion {fdlibm_ieee = -1, fdlibm_svid, fdlibm_xopen, fdlibm_posix};
  * s_lib_version.c) during compile time, it cannot be modified
  * in the middle of a program
  */ 
-__BEGIN_DECLS
- _LIB_VERSION_TYPE  _LIB_VERSION;
-__END_DECLS
+extern  _LIB_VERSION_TYPE  _LIB_VERSION;
 
 #define _IEEE_  fdlibm_ieee
 #define _SVID_  fdlibm_svid
@@ -102,84 +100,84 @@ struct exception {
 #define	PLOSS		6
 
 __BEGIN_DECLS
-int matherr __P((struct exception *));
+extern int matherr __P((struct exception *));
 
-int signgam;
+extern int signgam;
 
-double erf __P((double));
-double erfc __P((double));
-double gamma __P((double));
-int isnan __P((double));
-int finite __P((double));
-double j0 __P((double));
-double j1 __P((double));
-double jn __P((int, double));
-double lgamma __P((double));
-double y0 __P((double));
-double y1 __P((double));
-double yn __P((int, double));
-double acosh __P((double));
-double asinh __P((double));
-double cbrt __P((double));
-double nextafter __P((double, double));
-double remainder __P((double, double));
-double significand __P((double));
-double copysign __P((double, double));
-int ilogb __P((double));
-double scalbn __P((double, int));
+extern double erf __P((double));
+extern double erfc __P((double));
+extern double gamma __P((double));
+extern int isnan __P((double));
+extern int finite __P((double));
+extern double j0 __P((double));
+extern double j1 __P((double));
+extern double jn __P((int, double));
+extern double lgamma __P((double));
+extern double y0 __P((double));
+extern double y1 __P((double));
+extern double yn __P((int, double));
+extern double acosh __P((double));
+extern double asinh __P((double));
+extern double cbrt __P((double));
+extern double nextafter __P((double, double));
+extern double remainder __P((double, double));
+extern double significand __P((double));
+extern double copysign __P((double, double));
+extern int ilogb __P((double));
+extern double scalbn __P((double, int));
 /*
  * Reentrant version of gamma & lgamma; passes signgam back by reference
  * as the second argument; user must allocate space for signgam.
  */
 #ifdef _REENTRANT
-double gamma_r __P((double, int *));
-double lgamma_r __P((double, int *));
+extern double gamma_r __P((double, int *));
+extern double lgamma_r __P((double, int *));
 #endif	/* _REENTRANT */
 
 /* ieee style elementary functions */
-double __ieee754_sqrt __P((double));			
-double __ieee754_acos __P((double));			
-double __ieee754_acosh __P((double));			
-double __ieee754_log __P((double));			
-double __ieee754_atanh __P((double));			
-double __ieee754_asin __P((double));			
-double __ieee754_atan2 __P((double,double));			
-double __ieee754_exp __P((double));
-double __ieee754_cosh __P((double));
-double __ieee754_fmod __P((double,double));
-double __ieee754_pow __P((double,double));
-double __ieee754_lgamma_r __P((double,int *));
-double __ieee754_gamma_r __P((double,int *));
-double __ieee754_lgamma __P((double));
-double __ieee754_gamma __P((double));
-double __ieee754_log10 __P((double));
-double __ieee754_sinh __P((double));
-double __ieee754_hypot __P((double,double));
-double __ieee754_j0 __P((double));
-double __ieee754_j1 __P((double));
-double __ieee754_y0 __P((double));
-double __ieee754_y1 __P((double));
-double __ieee754_jn __P((int,double));
-double __ieee754_yn __P((int,double));
-double __ieee754_remainder __P((double,double));
-int    __ieee754_rem_pio2 __P((double,double*));
+extern double __ieee754_sqrt __P((double));			
+extern double __ieee754_acos __P((double));			
+extern double __ieee754_acosh __P((double));			
+extern double __ieee754_log __P((double));			
+extern double __ieee754_atanh __P((double));			
+extern double __ieee754_asin __P((double));			
+extern double __ieee754_atan2 __P((double,double));			
+extern double __ieee754_exp __P((double));
+extern double __ieee754_cosh __P((double));
+extern double __ieee754_fmod __P((double,double));
+extern double __ieee754_pow __P((double,double));
+extern double __ieee754_lgamma_r __P((double,int *));
+extern double __ieee754_gamma_r __P((double,int *));
+extern double __ieee754_lgamma __P((double));
+extern double __ieee754_gamma __P((double));
+extern double __ieee754_log10 __P((double));
+extern double __ieee754_sinh __P((double));
+extern double __ieee754_hypot __P((double,double));
+extern double __ieee754_j0 __P((double));
+extern double __ieee754_j1 __P((double));
+extern double __ieee754_y0 __P((double));
+extern double __ieee754_y1 __P((double));
+extern double __ieee754_jn __P((int,double));
+extern double __ieee754_yn __P((int,double));
+extern double __ieee754_remainder __P((double,double));
+extern int    __ieee754_rem_pio2 __P((double,double*));
 #define _SCALB_INT
 #ifdef _SCALB_INT
-double __ieee754_scalb __P((double,int));
+extern double __ieee754_scalb __P((double,int));
 #else
-double __ieee754_scalb __P((double,double));
+extern double __ieee754_scalb __P((double,double));
 #endif
 
 /* fdlibm kernel function */
-double __kernel_standard __P((double,double,int));	
-double __kernel_sin __P((double,double,int));
-double __kernel_cos __P((double,double));
-double __kernel_tan __P((double,double,int));
-int    __kernel_rem_pio2 __P((double*,double*,int,int,int,const int*));
+extern double __kernel_standard __P((double,double,int));	
+extern double __kernel_sin __P((double,double,int));
+extern double __kernel_cos __P((double,double));
+extern double __kernel_tan __P((double,double,int));
+extern int    __kernel_rem_pio2 __P((double*,double*,int,int,int,const int*));
 __END_DECLS
 
 #if (defined(__GNUC__) || defined(__cplusplus)) && defined(__HAVE_68881__)
-#include <math-68881.h>
+#include <bits/math-68881.h>
 #else
 
 #define	HUGE_VAL	1e500			/* IEEE: positive infinity */
@@ -191,52 +189,49 @@ __END_DECLS
 #define	HUGE	((float)3.40282346638528860e+38)
 
 __BEGIN_DECLS
-double acos __P((double));
-double asin __P((double));
-double atan __P((double));
-double atan2 __P((double, double));
-double cos __P((double));
-double sin __P((double));
-double tan __P((double));
-double cosh __P((double));
-double sinh __P((double));
-double tanh __P((double));
-double exp __P((double));
-double frexp __P((double, int *));
-double ldexp __P((double, int));
-double log __P((double));
-double log10 __P((double));
-double modf __P((double, double *));
-double pow __P((double, double));
-double sqrt __P((double));
-double ceil __P((double));
-double fabs __P((double));
-double floor __P((double));
-double fmod __P((double, double));
-double hypot __P((double, double));
-double atanh __P((double));
-double logb __P((double));
+extern double acos __P((double));
+extern double asin __P((double));
+extern double atan __P((double));
+extern double atan2 __P((double, double));
+extern double cos __P((double));
+extern double sin __P((double));
+extern double tan __P((double));
+extern double cosh __P((double));
+extern double sinh __P((double));
+extern double tanh __P((double));
+extern double exp __P((double));
+extern double frexp __P((double, int *));
+extern double ldexp __P((double, int));
+extern double log __P((double));
+extern double log10 __P((double));
+extern double modf __P((double, double *));
+extern double pow __P((double, double));
+extern double sqrt __P((double));
+extern double ceil __P((double));
+extern double fabs __P((double));
+extern double floor __P((double));
+extern double fmod __P((double, double));
+extern double hypot __P((double, double));
+extern double atanh __P((double));
+extern double logb __P((double));
 #ifdef _SCALB_INT
-double scalb __P((double, int));
+extern double scalb __P((double, int));
 #else
-double scalb __P((double, double));
+extern double scalb __P((double, double));
 #endif
-double rint __P((double));
-double expm1 __P((double));
-double log1p __P((double));
+extern double rint __P((double));
+extern double expm1 __P((double));
+extern double log1p __P((double));
 __END_DECLS
 
 #endif /* __HAVE_68881__ */
 
 __BEGIN_DECLS
 /* These functions are defined in ixemul */
-int     isinf __P((double));
-int     isnan __P((double));
+extern int     isinf __P((double));
+extern int     isnan __P((double));
 /* This function had a different name in the old libm.a */
 #define drem remainder
-/* These functions are missing from the library, but were defined
-   in the older libm.a */
-double  cabs();		/* we can't describe cabs()'s argument properly */
 __END_DECLS
 
 #endif /* _MATH_H_ */
