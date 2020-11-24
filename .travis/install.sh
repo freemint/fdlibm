8c1d8f218964e984e9913824072fd51677f74438
Miro Kropacek <miro.kropacek@gmail.com>
2020-11-24 18:35:34 +0100

    Use bintray snapshots

---
+++ .travis/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+# -x: Display expanded script commands
+
+./.travis/install_bintray.sh mintlib
