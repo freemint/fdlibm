49a52b7fb79b97c0878e9088237411322a34c9dd
Miro Kropacek <miro.kropacek@gmail.com>
2020-11-29 10:12:36 +0100

    Use latest binutils/gcc/mintbin

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,4 +3,12 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-./.travis/install_bintray.sh mintlib
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_REPO_SLUG}" = "freemint/fdlibm" ]
+then
+	BINTRAY_REPO="travis" SYSROOT_DIR="/" ./.travis/install_bintray.sh m68k-atari-mint-binutils-gdb m68k-atari-mint-gcc mintbin
+	./.travis/install_bintray.sh mintlib
+else
+	sudo add-apt-repository -y ppa:vriviere/ppa
+	sudo apt-get update
+	sudo apt-get install binutils-m68k-atari-mint gcc-m68k-atari-mint mintbin-m68k-atari-mint mintlib-m68k-atari-mint
+fi
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build Status](https://travis-ci.org/freemint/fdlibm.svg?branch=master)](https://travis-ci.org/freemint/fdlibm) [ ![Download](https://api.bintray.com/packages/freemint/freemint/fdlibm/images/download.svg) ](https://bintray.com/freemint/freemint/fdlibm/_latestVersion)
+[![Build Status](https://travis-ci.org/freemint/fdlibm.svg?branch=master)](https://travis-ci.org/freemint/fdlibm) [ ![Download](https://api.bintray.com/packages/freemint/lib/fdlibm/images/download.svg) ](https://bintray.com/freemint/lib/fdlibm/_latestVersion)
